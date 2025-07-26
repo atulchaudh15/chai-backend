cb64aec7bbb45c24f39560040953e94962cc7e1b
atulchaudh15 <atulchuadh9450@gmail.com>
2025-07-26 23:28:54 +0530

    Various utility code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,32 @@
+
+//METHOD-1(USING TRY- CATCH)
+/*const asyncHandler = (func)=> async(req,res,next)=> {
+     try {
+        await func(req,res,next)
+     } catch (error) {
+        res.status(error.code || 500).json({
+            success: false,
+            message: error.message
+        })
+     }
+}*/
+
+//isko bolte h higher order func mtlb ek func jo callback m b ek dusra func accept krleta h
+// const asyncHandler= ()=>{}
+// const asyncHandler= (func) => {
+// ()=>{}
+//    }
+// const asyncHandler= (func)=> ()=>{}
+
+
+
+//METHOD-2
+//ye wala promise ko use krke kiya h 
+const asyncHandler= (requestHandler)=> {
+    (req,res,next) => {
+        Promise.resolve().
+        catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
